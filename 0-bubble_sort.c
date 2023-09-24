4db43e632c063458f80aa6eb0ede5eeb2b860211
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-09-24 03:12:36 +0100

    fix: logical error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,12 @@
 #include "sort.h"
 
 /**
-* bubble_sort - sort an array of integers using bubble sort algorithm
-* @array: array of integers
-* @size: size of array
-*
-* Return: nothing
-*/
+ * bubble_sort - sort an array of integers using bubble sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t index, num_of_elem;
@@ -16,21 +16,22 @@ void bubble_sort(int *array, size_t size)
 		for (index = 0; index < num_of_elem - 1; index++)
 		{
 			if (array[index] > array[index + 1])
+			{
 				swap_elements(array, index, index + 1);
-
-			print_array(array, size);
+				print_array(array, size);
+			}
 		}
 	}
 }
 
 /**
-* swap_elements - swap two elements based on index
-* @array: pointer to original array
-* @index_1: the first element index
-* @index_2: the second element index
-*
-* Return: nothing
-*/
+ * swap_elements - swap two elements based on index
+ * @array: pointer to original array
+ * @index_1: the first element index
+ * @index_2: the second element index
+ *
+ * Return: nothing
+ */
 void swap_elements(int *array, size_t index_1, size_t index_2)
 {
 	int temporary_integer = array[index_1];
@@ -38,4 +39,3 @@ void swap_elements(int *array, size_t index_1, size_t index_2)
 	array[index_1] = array[index_2];
 	array[index_2] = temporary_integer;
 }
-
